fe41e39f556a2ec8346fbe1b7a6b4922c28e7f6d
unknown <ngochai06122002@gmail.com>
2025-04-12 16:14:46 +0700

    Finsh

---
+++ src/constants/index.js
@@ -0,0 +1,287 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" }, 
+  { text: "Business", imgPath: "/images/business.svg" },
+  { text: "Solutions", imgPath: "/images/solutions.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 2, suffix: "+", label: "Years of Experience" },
+  { value: 5, suffix: "+", label: "Satisfied Clients" },
+  { value: 7, suffix: "+", label: "Projects Analyzed and Delivered(More)" },
+  { value: 80, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.webp",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Requirement Clarity",
+    desc: "Transforming business needs into clear, actionable technical documentation.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "User-Centric Design",
+    desc: "Ensuring intuitive UX flows and interface design aligned with user needs.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "System Integration",
+    desc: "Connecting different systems seamlessly for better data flow and efficiency.",
+  },
+];
+
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Business Analyst",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Business Analyst",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    title: "Junior Functional Analyst – SADESA",
+    date: "Nov 2024 – Present",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    responsibilities: [
+      "Analyze and optimize business processes within JD Edwards EnterpriseOne.",
+      "Collaborate on UI/UX, data structure design, and system documentation.",
+      "Support end-users through training and troubleshooting.",
+      "Generate analytical reports for management decisions.",
+    ]
+  },
+  {
+    title: "Business Analyst – APETECH SOLUTIONS",
+    date: "Jan 2024 – Nov 2024",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    responsibilities: [
+      "Collected and analyzed business requirements for TMS & DMS systems.",
+      "Extracted insights from contracts and reports to design effective solutions.",
+      "Facilitated cross-functional communication between business and tech teams.",
+      "Documented user needs and addressed concerns proactively.",
+    ]
+  },
+  {
+    title: "Intern Business Analyst – 3FORCOM",
+    date: "Jul 2023 – Nov 2023",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    responsibilities: [
+      "Gathered and documented user requirements for event software.",
+      "Developed FRD/SRD documents aligned with project goals.",
+      "Tested software and ensured client expectations were met.",
+    ]
+  },
+  {
+    title: "Intern Business Analyst – GLOBALCHAIN",
+    date: "May 2023 – Jul 2023",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    responsibilities: [
+      "Designed UI/UX for SCHAT – internal chat for police force.",
+      "Created ERDs to optimize structure and communication flow.",
+      "Enhanced product quality via testing and issue tracking.",
+      "Contributed to marketing materials and product promotion.",
+    ]
+  }
+];
+
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Esther Howard",
+    mentions: "@estherhoward",
+    review:
+      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Wade Warren",
+    mentions: "@wadewarren",
+    review:
+      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Guy Hawkins",
+    mentions: "@guyhawkins",
+    review:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Floyd Miles",
+    mentions: "@floydmiles",
+    review:
+      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Albert Flores",
+    mentions: "@albertflores",
+    review:
+      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
